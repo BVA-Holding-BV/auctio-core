bf96367a41f8c4ca93026dcf1f71c808b325c31f
Berry Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-01-17 16:56:07 +0100

    Add Request-Params Class

--- src/RequestParams.php
+++ src/RequestParams.php
@@ -15,7 +15,7 @@ class RequestParams
     {
         $output = array();
 
-        $output['filters'] = (isset($params->filters)) ? json_decode($params->filters, true) : null;
+        $output['filter'] = (isset($params->filter)) ? json_decode($params->filter, true) : null;
         $output['orderBy'] = (isset($params->orderBy)) ? json_decode($params->orderBy, true) : null;
         $output['limit'] = (isset($params->limit)) ? $params->limit : null;
         $output['offset'] = (isset($params->page)) ? ($params->page - 1) * $output['limit'] : null;
