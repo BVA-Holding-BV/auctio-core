57dccb30d5c7dc732eb5d0bf386c13f64e690c57
Berry Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-05-30 11:38:22 +0200

    Add convertJson-function

--- src/Input.php
+++ src/Input.php
@@ -6,23 +6,30 @@ class Input
 {
 
     /**
-     * Set parameters by request-object
+     * Convert JSON-string (or object/array) into array/object
      *
-     * @param $params
-     * @return array
+     * @param $dataString
+     * @param string $output
+     * @return mixed|void
      */
-    public function setParams($params)
+    public function convertJson($dataString, $output = "object")
     {
-        $output = array();
+        if (!is_object($dataString) && !is_array($dataString)) {
+            $dataString = str_replace("'", '"', $dataString);
+            if ($this->isJson($dataString)) {
+                $data = $dataString;
+            } else {
+                return;
+            }
+        } else {
+            $data = json_encode($dataString);
+        }
 
-        $output['filter'] = (isset($params->filter)) ? json_decode($params->filter, true) : null;
-        $output['orderBy'] = (isset($params->orderBy)) ? json_decode($params->orderBy, true) : null;
-        $output['limit'] = (isset($params->limit)) ? $params->limit : null;
-        $output['offset'] = (isset($params->page)) ? ($params->page - 1) * $output['limit'] : null;
-        $output['debug'] = (isset($params->debug) && ($params->debug == 'true' || $params->debug == 1)) ? true : false;
-        $output['customRequestId'] = (isset($params->customRequestId) && !empty($params->customRequestId)) ? $params->customRequestId : null;
-
-        return $output;
+        if ($output == "object") {
+            return json_decode($data);
+        } else {
+            return json_decode($data, true);
+        }
     }
 
     /**
@@ -50,12 +57,33 @@ class Input
     /**
      * Check if string is JSON-string
      *
-     * @param string $string
+     * @param string $dataString
      * @return boolean
      */
-    public function isJson($string)
+    public function isJson($dataString)
     {
-        return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
+        return is_string($dataString) && is_array(json_decode($dataString, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
+    }
+
+
+    /**
+     * Set parameters by request-object
+     *
+     * @param $params
+     * @return array
+     */
+    public function setParams($params)
+    {
+        $output = array();
+
+        $output['filter'] = (isset($params->filter)) ? json_decode($params->filter, true) : null;
+        $output['orderBy'] = (isset($params->orderBy)) ? json_decode($params->orderBy, true) : null;
+        $output['limit'] = (isset($params->limit)) ? $params->limit : null;
+        $output['offset'] = (isset($params->page)) ? ($params->page - 1) * $output['limit'] : null;
+        $output['debug'] = (isset($params->debug) && ($params->debug == 'true' || $params->debug == 1)) ? true : false;
+        $output['customRequestId'] = (isset($params->customRequestId) && !empty($params->customRequestId)) ? $params->customRequestId : null;
+
+        return $output;
     }
 
 }
