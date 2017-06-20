ba6f86064a1997fc7df7502b00c4df4ba32da89e
Berry Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-06-20 10:36:21 +0200

    Add group-by and having to (default) parameters

--- src/Input.php
+++ src/Input.php
@@ -77,6 +77,8 @@ class Input
         $output = array();
 
         $output['filter'] = (isset($params->filter)) ? json_decode($params->filter, true) : null;
+        $output['groupBy'] = (isset($params->groupBy)) ? json_decode($params->groupBy, true) : null;
+        $output['having'] = (isset($params->having)) ? json_decode($params->having, true) : null;
         $output['orderBy'] = (isset($params->orderBy)) ? json_decode($params->orderBy, true) : null;
         $output['limit'] = (isset($params->limit)) ? $params->limit : null;
         $output['offset'] = (isset($params->page)) ? ($params->page - 1) * $output['limit'] : null;
