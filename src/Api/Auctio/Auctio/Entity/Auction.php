034d2b4e75deb7f6fdd8c26dc449d2213f3a5a10
Berry Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-06-22 10:32:53 +0200

    Add entities to Auctio-API

---
+++ src/Api/Auctio/Auctio/Entity/Auction.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace AuctioCore\Api\Auctio\Entity;
+
+use AuctioCore\Api\Auctio\Entity\Abs\Base;
+
+class Auction extends Base {
+
+    /**
+     * @var int
+     */
+    public $id;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $name;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $description;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    //public $keywords;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $auctionClosedText;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $bidRestrictionExplanationText;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $extraExplanationText;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $importantInformation;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $auctionAdvertisement;
+    /**
+     * @var float
+     */
+    public $bidLimitDispensationAmount;
+    /**
+     * @var int
+     */
+    public $countryId;
+    /**
+     * @var Custom\DateTime
+     */
+    public $creationDate;
+    /**
+     * @var Custom\DateTime
+     */
+    public $startDate;
+    /**
+     * @var Custom\DateTime
+     */
+    public $endDate;
+    /**
+     * @var bool
+     */
+    public $onlyBusinessCustomersCanBid;
+    /**
+     * @var bool
+     */
+    public $onlyLocalCustomersCanBid;
+    /**
+     * @var bool
+     */
+    public $privateAuction;
+    /**
+     * @ReadOnly
+     */
+    public $privateAuctionText;
+    /**
+     * @ReadOnly
+     */
+    public $privateAuctionApplicationEmail;
+    /**
+     * @var bool
+     */
+    public $showPrivateAuctionOnHomepage;
+    /**
+     * @var bool
+     */
+    public $allowPrivateAuctionApplications;
+    /**
+     * @var bool
+     */
+    public $allowAutomaticPrivateAuctionAccess;
+    /**
+     * @var bool
+     */
+    public $showBanners;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $extraTermsText;
+    /**
+     * @var bool
+     */
+    public $extraTermsRequired;
+    /**
+     * @var bool
+     */
+    public $bidLimitRequired;
+    /**
+     * @var bool
+     */
+    public $active;
+    /**
+     * @ReadOnly
+     */
+    public $termsUrl;
+    public $leafletURL;
+    public $remark;
+    /**
+     * @var int
+     */
+    public $disableBidding;
+    /**
+     * @var Custom\LocaleMessage
+     */
+    public $themeIntroduction;
+    /**
+     * @var bool
+     */
+    public $permissionRequired;
+    /**
+     * @var int
+     */
+    public $bidRangeId;
+    /**
+     * @var int
+     */
+    public $responsibilityUnitId;
+    /**
+     * @var array
+     */
+    public $languages;
+    /**
+     * @var array
+     */
+    public $channelCodes;
+    /**
+     * @var int
+     */
+    public $reverseBidRangeId;
+    /**
+     * @var string
+     * @ReadOnly
+     */
+    public $createdBy;
+}
---
+++ src/Api/Auctio/Auctio/Entity/Custom/LocaleMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AuctioCore\Api\Auctio\Entity\Custom;
+
+use AuctioCore\Api\Auctio\Defaults;
+use AuctioCore\Api\Auctio\Entity\Abs\Base;
+
+class LocaleMessage extends Base {
+
+    /**
+	 * @var string
+	 */
+	public $nl;
+	/**
+	 * @var string
+	 */
+	public $en;
+	/**
+	 * @var string
+	 */
+	public $es;
+	/**
+	 * @var string
+	 */
+	public $de;
+
+	public function encode() {
+		$data = array();
+		$encoded = parent::encode();
+
+		foreach(json_decode($encoded) as $language => $value) {
+			if($value !== null) {
+				$data[$language] = $value;
+			}
+		}
+
+		if(empty($data)) {
+			$data = new \stdClass();
+		}
+
+		return json_encode($data);
+	}
+
+	public function exchangeArray($data) {
+		$this->nl = (!empty($data['nl'])) ? $data['nl'] : null;
+		$this->en = (!empty($data['en'])) ? $data['en'] : null;
+		$this->es = (!empty($data['es'])) ? $data['es'] : null;
+		$this->de = (!empty($data['de'])) ? $data['de'] : null;
+	}
+
+}
