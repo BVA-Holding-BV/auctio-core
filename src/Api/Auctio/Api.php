fa5b66ebe5d2c7c70d4f8bbe32f09b59ebbfa684
B.Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-08-21 13:59:28 +0200

    Add get location by id function

--- src/Api/Auctio/Api.php
+++ src/Api/Auctio/Api.php
@@ -525,6 +525,32 @@ class Api
         }
     }
 
+    public function getAuctionLocation($locationId)
+    {
+        // Prepare request
+        $requestHeader = $this->clientHeaders;
+
+        // Execute request
+        $result = $this->client->request('GET', 'ext123/location/' . $locationId . "", ["headers"=>$requestHeader]);
+        if ($result->getStatusCode() == 200) {
+            $response = json_decode((string) $result->getBody());
+
+            // Return
+            if (!isset($response->errors)) {
+                return $response;
+            } else {
+                $this->setErrorData($response);
+                $this->setMessages($response->errors);
+                return false;
+            }
+        } else {
+            $response = json_decode((string) $result->getBody());
+            $this->setErrorData($response);
+            $this->setMessages([$result->getStatusCode() . ": " . $result->getReasonPhrase()]);
+            return false;
+        }
+    }
+
     public function getAuctionMainCategories($auctionId)
     {
         // Prepare request
