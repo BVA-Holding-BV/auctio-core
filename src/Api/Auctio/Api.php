53dcdef39ea3cb4ee8674489569554af5ab2ba3d
B.Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-09-06 14:58:59 +0200

    Add update main/sub-categories (Auctio API)

--- src/Api/Auctio/Api.php
+++ src/Api/Auctio/Api.php
@@ -499,6 +499,58 @@ class Api
         }
     }
 
+    public function updateMainCategory(\AuctioCore\Api\Auctio\Entity\MainCategory $mainCategory)
+    {
+        // Prepare request
+        $requestHeader = $this->clientHeaders;
+
+        // Execute request
+        $result = $this->client->request('POST', 'ext123/lotmaincategory', ["headers"=>$requestHeader, "body"=>$mainCategory->encode()]);
+        if ($result->getStatusCode() == 200) {
+            $response = json_decode((string) $result->getBody());
+
+            // Return
+            if (!isset($response->errors)) {
+                return $response;
+            } else {
+                $this->setErrorData($response);
+                $this->setMessages($response->errors);
+                return false;
+            }
+        } else {
+            $response = json_decode((string) $result->getBody());
+            $this->setErrorData($response);
+            $this->setMessages([$result->getStatusCode() . ": " . $result->getReasonPhrase()]);
+            return false;
+        }
+    }
+
+    public function updateSubCategory(\AuctioCore\Api\Auctio\Entity\SubCategory $subCategory)
+    {
+        // Prepare request
+        $requestHeader = $this->clientHeaders;
+
+        // Execute request
+        $result = $this->client->request('POST', 'ext123/lotsubcategory', ["headers"=>$requestHeader, "body"=>$subCategory->encode()]);
+        if ($result->getStatusCode() == 200) {
+            $response = json_decode((string) $result->getBody());
+
+            // Return
+            if (!isset($response->errors)) {
+                return $response;
+            } else {
+                $this->setErrorData($response);
+                $this->setMessages($response->errors);
+                return false;
+            }
+        } else {
+            $response = json_decode((string) $result->getBody());
+            $this->setErrorData($response);
+            $this->setMessages([$result->getStatusCode() . ": " . $result->getReasonPhrase()]);
+            return false;
+        }
+    }
+
     public function deleteCollectionDay($dayId)
     {
         // Prepare request
