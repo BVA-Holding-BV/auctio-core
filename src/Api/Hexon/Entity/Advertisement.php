ab79ecd27617f76ebeb105bbce0c9ad4aa6e7df5
B.Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-10-02 13:21:48 +0200

    Add Hexon-API functionalities

--- src/Api/Hexon/Entity/Advertisement.php
+++ src/Api/Hexon/Entity/Advertisement.php
@@ -4,11 +4,11 @@ namespace AuctioCore\Api\Hexon\Entity;
 
 use AuctioCore\Api\Base;
 
-class Vehicle extends Base {
+class Advertisement extends Base {
 
     /** @var string */
     public $stocknumber;
-    /** @var boolean */
-    public $identification_stocknumber_public;
+    /** @var string */
+    public $site_code;
 
 }
