dc1e65815317a76c2929c297da2b0c2315063cdf
B.Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-10-23 14:37:31 +0200

    Send mail by EWS

--- src/Mail/EWS/Mail.php
+++ src/Mail/EWS/Mail.php
@@ -3,17 +3,20 @@
 namespace AuctioCore\Mail\EWS;
 
 use \jamesiarmes\PhpEws\Client;
+use \jamesiarmes\PhpEws\Request\CreateItemType;
 use \jamesiarmes\PhpEws\Request\SendItemType;
 
 use \jamesiarmes\PhpEws\ArrayType\ArrayOfRecipientsType;
-
+use \jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfAllItemsType;
 use \jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfBaseItemIdsType;
 
 use \jamesiarmes\PhpEws\Enumeration\BodyTypeType;
+use \jamesiarmes\PhpEws\Enumeration\MessageDispositionType;
 use \jamesiarmes\PhpEws\Enumeration\ResponseClassType;
 
 use \jamesiarmes\PhpEws\Type\BodyType;
 use \jamesiarmes\PhpEws\Type\EmailAddressType;
+use \jamesiarmes\PhpEws\Type\ItemIdType;
 use \jamesiarmes\PhpEws\Type\MessageType;
 use \jamesiarmes\PhpEws\Type\SingleRecipientType;
 
@@ -21,7 +24,7 @@ class Mail
 {
 
     private $client;
-    private $username;
+    private $sender;
     private $messages;
     private $errorData;
 
@@ -33,11 +36,11 @@ class Mail
      * @param string $password
      * @param string $version
      */
-    public function __construct($hostname, $username, $password, $version = 'Exchange2010')
+    public function __construct($hostname, $username, $password, $senderMailAddress, $version = 'Exchange2010')
     {
         // Set client
         $this->client = new Client($hostname, $username, $password, $version);
-        $this->username = $username;
+        $this->sender = $senderMailAddress;
 
         // Set error-messages
         $this->messages = [];
@@ -97,10 +100,10 @@ class Mail
         return $this->messages;
     }
 
-    public function send($recipient, $subject = NULL, $content, $saveToFolder = true)
+    public function send($mailRecipient, $subject = NULL, $content, $saveToFolder = true)
     {
         // Check input-data
-        if (empty($recipient)) {
+        if (empty($mailRecipient)) {
             $this->setMessages("No recipient set");
             return false;
         } elseif (empty($subject)) {
@@ -111,36 +114,77 @@ class Mail
             return false;
         }
 
-        // Build the request.
-        $request = new SendItemType();
+        // Build the request,
+        $request = new CreateItemType();
         $request->SaveItemToFolder = ($saveToFolder === false) ? false : true;
-        $request->ItemIds = new NonEmptyArrayOfBaseItemIdsType();
+        $request->Items = new NonEmptyArrayOfAllItemsType();
 
-        // Create the message.
+        // Save the message, but do not send it
+        $request->MessageDisposition = MessageDispositionType::SAVE_ONLY;
+
+        // Create the message
         $message = new MessageType();
         $message->Subject = $subject;
         $message->ToRecipients = new ArrayOfRecipientsType();
 
-        // Set the sender.
-        $senderMailAddress = str_ireplace("bva\\", "", $this->username) . "@bva-auctions.com";
+        // Set the sender
         $message->From = new SingleRecipientType();
         $message->From->Mailbox = new EmailAddressType();
-        $message->From->Mailbox->EmailAddress = $senderMailAddress;
+        $message->From->Mailbox->EmailAddress = $this->sender;
 
-        // Set the recipient.
+        // Set the recipient
         $recipient = new EmailAddressType();
-        $recipient->EmailAddress = $recipient;
+        $recipient->EmailAddress = $mailRecipient;
         $message->ToRecipients->Mailbox[] = $recipient;
 
-        // Set the message body.
+        // Set the message body
         $message->Body = new BodyType();
         $message->Body->BodyType = BodyTypeType::TEXT;
         $message->Body->_ = $content;
 
-        // Add the message to the request.
+        // Add the message to the request
         $request->Items->Message[] = $message;
+        $response = $this->client->CreateItem($request);
 
-        // Sent message
+        // Iterate over the results
+        $response_messages = $response->ResponseMessages->CreateItemResponseMessage;
+        foreach ($response_messages as $response_message) {
+            // Make sure the request succeeded.
+            if ($response_message->ResponseClass != ResponseClassType::SUCCESS) {
+                $this->setMessages($response_message->ResponseCode . ": " . $response_message->MessageText);
+                return false;
+            }
+
+            // Iterate over the created messages, printing the id for each.
+            foreach ($response_message->Items->Message as $item) {
+                return $this->sendMail($item->ItemId->Id, $item->ItemId->ChangeKey);
+            }
+        }
+    }
+
+    private function sendMail($messageId, $changeKey)
+    {
+        // Check input-data
+        if (empty($messageId)) {
+            $this->setMessages("No message-id set");
+            return false;
+        } elseif (empty($changeKey)) {
+            $this->setMessages("No change-key set");
+            return false;
+        }
+
+        // Build the request
+        $request = new SendItemType();
+        $request->SaveItemToFolder = true;
+        $request->ItemIds = new NonEmptyArrayOfBaseItemIdsType();
+
+        // Add the message to the request
+        $item = new ItemIdType();
+        $item->Id = $messageId;
+        $item->ChangeKey = $changeKey;
+        $request->ItemIds->ItemId[] = $item;
+
+        // Sent message to
         $response = $this->client->SendItem($request);
 
         // Iterate over the results
@@ -151,10 +195,7 @@ class Mail
                 $this->setMessages($response_message->ResponseCode . ": " . $response_message->MessageText);
                 return false;
             }
-
-            // Return true (message sent successfully)
             return true;
         }
     }
-
 }
