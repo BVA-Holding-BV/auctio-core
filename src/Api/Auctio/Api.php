ac398ca5d1f27f82b569f7ff86129dbac65a24b9
B.Buitenhuis <b.buitenhuis@bva-auctions.com>
2017-10-26 14:39:29 +0200

    Add delete lot-media function to Auctio-API

--- src/Api/Auctio/Api.php
+++ src/Api/Auctio/Api.php
@@ -616,6 +616,32 @@ class Api
         }
     }
 
+    public function deleteLotMedia($lotId, $imageName)
+    {
+        // Prepare request
+        $requestHeader = $this->clientHeaders;
+
+        // Execute request
+        $result = $this->client->request('DELETE', 'ext123/lotmedia/' . $lotId . '/' . $imageName, ["headers"=>$requestHeader]);
+        if ($result->getStatusCode() == 200) {
+            $response = json_decode((string) $result->getBody());
+
+            // Return
+            if (!isset($response->errors)) {
+                return $response;
+            } else {
+                $this->setErrorData($response);
+                $this->setMessages($response->errors);
+                return false;
+            }
+        } else {
+            $response = json_decode((string) $result->getBody());
+            $this->setErrorData($response);
+            $this->setMessages([$result->getStatusCode() . ": " . $result->getReasonPhrase()]);
+            return false;
+        }
+    }
+
     public function getAuction($auctionId)
     {
         // Prepare request
