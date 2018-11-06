62cfdbf3028c430df8d4fb99d944a179505ed9f8
B. Buitenhuis <b.buitenhuis@bva-auctions.com>
2018-11-06 09:59:14 +0100

    Add negative-values validation (optional) to integer-input-validation

--- src/Zf/InputFilter/IntegerInputFilter.php
+++ src/Zf/InputFilter/IntegerInputFilter.php
@@ -11,21 +11,37 @@ class IntegerInputFilter
      * Get InputFilter for a Integer-type field
      *
      * @param $name
+     * @param bool $allow_negative_values
      * @param bool $required
      * @return void|InputFilter
      */
-    public function getFilter($name, $required = false)
+    public function getFilter($name, $allow_negative_values = false, $required = false)
     {
         if ($name == null) {
             return;
         } else {
-            $filter = [
-                'name' => $name,
-                'required' => $required,
-                'validators' => [
-                    ['name' => 'Digits'],
-                ],
-            ];
+            if ($allow_negative_values === true) {
+                $filter = [
+                    'name' => $name,
+                    'required' => $required,
+                    'validators' => [
+                        [
+                            'name' => 'Regex',
+                            'options' => [
+                                'pattern' => '/^[-]?\d*$/',
+                            ],
+                        ],
+                    ],
+                ];
+            } else {
+                $filter = [
+                    'name' => $name,
+                    'required' => $required,
+                    'validators' => [
+                        ['name' => 'Digits'],
+                    ],
+                ];
+            }
 
             $inputFilter = new InputFilter();
             $inputFilter->add($filter, $name);
